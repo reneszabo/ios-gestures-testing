625b5ef44d700d1981aeb48b3b30e3e6b264fb00
Rene Ramirez <asus@renes-macbook-pro.local>
2015-10-19 13:21:22 -0700

    Initial Commit

---
+++ com.ramirez-portfolio.gesturesTests/com_ramirez_portfolio_gesturesTests.swift
@@ -0,0 +1,36 @@
+//
+//  com_ramirez_portfolio_gesturesTests.swift
+//  com.ramirez-portfolio.gesturesTests
+//
+//  Created by Rene Ramirez on 2015-10-19.
+//  Copyright © 2015 Rene Ramirez. All rights reserved.
+//
+
+import XCTest
+@testable import com_ramirez_portfolio_gestures
+
+class com_ramirez_portfolio_gesturesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
